581d65cd4d1fb9d16666c0df67a4d4a6e4738f48
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-14 00:12:17 -0300

    a

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,6 @@
 export const env = {
     DATABASE_URL: process.env.DATABASE_URL ?? "",
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY ?? "",
-    NEXT_PUBLIC_API_URL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "",
 };
 
