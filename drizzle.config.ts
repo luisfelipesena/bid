6f2e69660a82140154c849896c093402aaeeabf5
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-14 01:40:15 -0300

    fix all

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,10 @@ import { env } from "@/env"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-  out: "./migrations",
+  out: "./drizzle",
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  verbose: true,
-  strict: true,
 })
