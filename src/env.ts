7ac43cfe6646f62bb860d7ede74752846dda0fde
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 23:47:41 -0300

    trying

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,16 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-
-console.log(process.env);
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string(),
   },
   client: {
-    NEXT_PUBLIC_API_URL: z.string().url().default("http://localhost:3000"),
+    NEXT_PUBLIC_API_URL: z.string().url(),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
 });
