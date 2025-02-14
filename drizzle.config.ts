60b94e20bed0bbc6dd7f242d897d1c4a20d3da54
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-14 00:21:54 -0300

    a

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
 import { env } from "@/env"
 import { defineConfig } from "drizzle-kit"
 
-console.log({env: env.DATABASE_URL});
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/server/db/schema.ts",
