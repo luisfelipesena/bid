a1e849ef66996a69015b8bbabb821e896d536782
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-14 00:18:53 -0300

    use migrate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import { env } from "@/env"
 import { defineConfig } from "drizzle-kit"
 
+console.log({env: env.DATABASE_URL});
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/server/db/schema.ts",
